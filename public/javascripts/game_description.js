02f9513cee46ab1db52acaa74589cf0ace35046c
Guillaume Malette-Campeau <gmalette@gmail.com>
2011-03-09 20:39:22 -0500

    Description of the flow of a game.

---
+++ public/javascripts/game_description.js
@@ -0,0 +1,51 @@
+{
+	"infos": {
+		"id": 1,
+		"map_id": 1,
+		"start_time": 1029830918,
+		"end_time": 209490128,
+		"nb_turns": 23,
+		"players": [
+			{
+				"id": 1,
+				"ai_id": 4,
+				"ai_version": "1.3.311231231"
+			},
+			{
+				"id": 2,
+				"ai_id": 4,
+				"ai_version": "1.3.311231231"
+			},
+			{
+				"id": 3,
+				"ai_id": 4,
+				"ai_version": "1.3.311231231"
+			}
+		],
+	},
+	
+	"moves": [
+		{
+			"turn": 0,
+			"changes": [
+				{
+					"player_id": 1,
+					"from": 1,
+					"to": 2,
+					"nb_soldiers": 6
+				}
+			]
+		},
+		{
+			"turn": 2,
+			"changes": [
+				{
+					"player_id": 1,
+					"from": 1,
+					"to": 2,
+					"nb_soldiers": 6
+				}
+			]
+		}
+	]
+}
