3ac18a03684913fdbf317eee2bd5cb9af0572a41
Guillaume Malette <gmalette@gmail.com>
2011-03-10 20:33:20 -0500

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BerlinWeb::Application.config.secret_token = '0acb76b846b8fb0032d49fd95370cffeb9e0e0dcb9fdd95cfbff654041528621b9a8b5f2f80365f9ce79ee5a32be359b39ac25e0239f5b08c1553410ef9aa1f8'
