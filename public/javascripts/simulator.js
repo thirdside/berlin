15f56f9f6bb9f67fed5991f663f7cfb93333a54a
Guillaume Malette <gmalette@gmail.com>
2011-03-10 21:48:08 -0500

    Timer class

--- public/javascripts/simulator.js
+++ public/javascripts/simulator.js
@@ -54,6 +54,59 @@ var TS = TS || Class.create({}, {
 	}
 });
 
+/**
+ * Timer Util Class
+ * @see TS
+ */
+TS.Timer = Class.create(TS, {
+	initialize: function ($super)
+	{
+		$super();
+		this.stop();
+	},
+	
+	/**
+	 * Starts the timer
+	 * delay: time in ms between ticks 
+	 * repeat: number of times to run, negative for infinite. 
+	 */
+	start: function (delay, repeat)
+	{
+		this.stop();
+		var d			= new Date();
+		this.startTime	= d.getTime();
+		this.repeat		= repeat;
+		this.delay		= delay;
+		this.interval	= setInterval(this.onTick.bind(this), this.delay);
+	},
+	
+	onTick: function ()
+	{
+		var d = new Date();
+		this.lastTick = d.getTime();
+		this.tickCount++;
+		
+		this.fire("timer", {time: this.lastTick, count: this.tickCount})
+		
+		if (this.tickCount >= this.repeat && this.repeat > 0)
+		{
+			this.stop();
+		}
+	},
+	
+	stop: function ()
+	{
+		if (this.interval)
+			clearInterval(this.interval);
+			
+		this.interval	= null;
+		this.startTime	= null;
+		this.lastTick	= null;
+		this.tickCount	= 0;
+		this.repeat		= 0;
+	}
+});
+
 TS.Node = Class.create(TS, {
 	initialize: function ($super, id, type, x, y)
 	{
