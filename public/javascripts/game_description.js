551af943462be8a610ff157933c28588c470a93c
Guillaume Malette <gmalette@gmail.com>
2011-03-19 21:03:00 -0400

    Updated game description including spawns.

--- public/javascripts/game_description.js
+++ public/javascripts/game_description.js
@@ -1,50 +1,372 @@
 {
-	"infos": {
-		"id": 1,
-		"map_id": 1,
-		"start_time": 1029830918,
-		"end_time": 209490128,
-		"nb_turns": 23,
-		"players": [
-			{
-				"id": 1,
-				"ai_id": 4,
-				"ai_version": "1.3.311231231",
-				"name": "Toto"
-			},
-			{
-				"id": 2,
-				"ai_id": 4,
-				"ai_version": "1.3.311231231",
-				"name": "Toto"
-			},
-			{
-				"id": 3,
-				"ai_id": 4,
-				"ai_version": "1.3.311231231",
-				"name": "Toto"
-			}
-		]
-	},
-	
-	"turns": {
-        "0": {
-            "moves": [
-                {
-                    "player_id": 1,
-					"from": 1,
-					"to": 2,
-					"number_of_soldiers": 6
-                }
-            ],
-
-            "states": [
-                {
-                    "node_id": 1,
-                    "number_of_soldiers": 100,
-                    "player_id": 1
-                }
-            ]
+    "infos": {
+        "game_id": "aa8798f8-1277-4e2a-b391-459b7e63919e",
+        "map_id": 1,
+        "turn": 11,
+        "maximum_number_of_turns": 10,
+        "players": [{
+            "id": "0",
+            "name": "http://localhost:4568/onturn"
+        },
+        {
+            "id": "1",
+            "name": "http://localhost:4567/onturn"
+        }]
+    },
+    "turns": {
+        "1": {
+            "moves": [{
+                "player_id": "1",
+                "number_of_soldiers": 6,
+                "from": 4,
+                "to": 1
+            }],
+            "states": [{
+                "node_id": 1,
+                "player_id": "0",
+                "number_of_soldiers": 15
+            },
+            {
+                "node_id": 2,
+                "player_id": null,
+                "number_of_soldiers": 0
+            },
+            {
+                "node_id": 3,
+                "player_id": null,
+                "number_of_soldiers": 0
+            },
+            {
+                "node_id": 4,
+                "player_id": "1",
+                "number_of_soldiers": 10
+            }],
+            "spawns": [{
+                "player_id": "0",
+                "number_of_soldiers": 1,
+                "node_id": 1
+            }]
+        },
+        "2": {
+            "moves": [{
+                "player_id": "1",
+                "number_of_soldiers": 2,
+                "from": 4,
+                "to": 2
+            }],
+            "states": [{
+                "node_id": 1,
+                "player_id": "0",
+                "number_of_soldiers": 10
+            },
+            {
+                "node_id": 2,
+                "player_id": null,
+                "number_of_soldiers": 0
+            },
+            {
+                "node_id": 3,
+                "player_id": null,
+                "number_of_soldiers": 0
+            },
+            {
+                "node_id": 4,
+                "player_id": "1",
+                "number_of_soldiers": 4
+            }],
+            "spawns": [{
+                "player_id": "0",
+                "number_of_soldiers": 1,
+                "node_id": 1
+            }]
+        },
+        "3": {
+            "moves": [{
+                "player_id": "1",
+                "number_of_soldiers": 2,
+                "from": 2,
+                "to": 3
+            }],
+            "states": [{
+                "node_id": 1,
+                "player_id": "0",
+                "number_of_soldiers": 11
+            },
+            {
+                "node_id": 2,
+                "player_id": "1",
+                "number_of_soldiers": 2
+            },
+            {
+                "node_id": 3,
+                "player_id": null,
+                "number_of_soldiers": 0
+            },
+            {
+                "node_id": 4,
+                "player_id": "1",
+                "number_of_soldiers": 2
+            }],
+            "spawns": [{
+                "player_id": "0",
+                "number_of_soldiers": 1,
+                "node_id": 1
+            }]
+        },
+        "4": {
+            "moves": [{
+                "player_id": "1",
+                "number_of_soldiers": 2,
+                "from": 3,
+                "to": 4
+            }],
+            "states": [{
+                "node_id": 1,
+                "player_id": "0",
+                "number_of_soldiers": 12
+            },
+            {
+                "node_id": 2,
+                "player_id": "1",
+                "number_of_soldiers": 0
+            },
+            {
+                "node_id": 3,
+                "player_id": "1",
+                "number_of_soldiers": 2
+            },
+            {
+                "node_id": 4,
+                "player_id": "1",
+                "number_of_soldiers": 2
+            }],
+            "spawns": [{
+                "player_id": "0",
+                "number_of_soldiers": 1,
+                "node_id": 1
+            }]
+        },
+        "5": {
+            "moves": [{
+                "player_id": "1",
+                "number_of_soldiers": 4,
+                "from": 4,
+                "to": 2
+            }],
+            "states": [{
+                "node_id": 1,
+                "player_id": "0",
+                "number_of_soldiers": 13
+            },
+            {
+                "node_id": 2,
+                "player_id": "1",
+                "number_of_soldiers": 0
+            },
+            {
+                "node_id": 3,
+                "player_id": "1",
+                "number_of_soldiers": 0
+            },
+            {
+                "node_id": 4,
+                "player_id": "1",
+                "number_of_soldiers": 4
+            }],
+            "spawns": [{
+                "player_id": "0",
+                "number_of_soldiers": 1,
+                "node_id": 1
+            }]
+        },
+        "6": {
+            "moves": [{
+                "player_id": "1",
+                "number_of_soldiers": 1,
+                "from": 2,
+                "to": 3
+            }],
+            "states": [{
+                "node_id": 1,
+                "player_id": "0",
+                "number_of_soldiers": 14
+            },
+            {
+                "node_id": 2,
+                "player_id": "1",
+                "number_of_soldiers": 4
+            },
+            {
+                "node_id": 3,
+                "player_id": "1",
+                "number_of_soldiers": 0
+            },
+            {
+                "node_id": 4,
+                "player_id": "1",
+                "number_of_soldiers": 0
+            }],
+            "spawns": [{
+                "player_id": "0",
+                "number_of_soldiers": 1,
+                "node_id": 1
+            }]
+        },
+        "7": {
+            "moves": [{
+                "player_id": "1",
+                "number_of_soldiers": 2,
+                "from": 2,
+                "to": 1
+            }],
+            "states": [{
+                "node_id": 1,
+                "player_id": "0",
+                "number_of_soldiers": 15
+            },
+            {
+                "node_id": 2,
+                "player_id": "1",
+                "number_of_soldiers": 3
+            },
+            {
+                "node_id": 3,
+                "player_id": "1",
+                "number_of_soldiers": 1
+            },
+            {
+                "node_id": 4,
+                "player_id": "1",
+                "number_of_soldiers": 0
+            }],
+            "spawns": [{
+                "player_id": "0",
+                "number_of_soldiers": 1,
+                "node_id": 1
+            }]
+        },
+        "8": {
+            "moves": [{
+                "player_id": "1",
+                "number_of_soldiers": 1,
+                "from": 2,
+                "to": 3
+            }],
+            "states": [{
+                "node_id": 1,
+                "player_id": "0",
+                "number_of_soldiers": 14
+            },
+            {
+                "node_id": 2,
+                "player_id": "1",
+                "number_of_soldiers": 1
+            },
+            {
+                "node_id": 3,
+                "player_id": "1",
+                "number_of_soldiers": 1
+            },
+            {
+                "node_id": 4,
+                "player_id": "1",
+                "number_of_soldiers": 0
+            }],
+            "spawns": [{
+                "player_id": "0",
+                "number_of_soldiers": 1,
+                "node_id": 1
+            }]
+        },
+        "9": {
+            "moves": [{
+                "player_id": "1",
+                "number_of_soldiers": 2,
+                "from": 3,
+                "to": 2
+            }],
+            "states": [{
+                "node_id": 1,
+                "player_id": "0",
+                "number_of_soldiers": 15
+            },
+            {
+                "node_id": 2,
+                "player_id": "1",
+                "number_of_soldiers": 0
+            },
+            {
+                "node_id": 3,
+                "player_id": "1",
+                "number_of_soldiers": 2
+            },
+            {
+                "node_id": 4,
+                "player_id": "1",
+                "number_of_soldiers": 0
+            }],
+            "spawns": [{
+                "player_id": "0",
+                "number_of_soldiers": 1,
+                "node_id": 1
+            }]
+        },
+        "10": {
+            "moves": [{
+                "player_id": "1",
+                "number_of_soldiers": 1,
+                "from": 2,
+                "to": 4
+            }],
+            "states": [{
+                "node_id": 1,
+                "player_id": "0",
+                "number_of_soldiers": 16
+            },
+            {
+                "node_id": 2,
+                "player_id": "1",
+                "number_of_soldiers": 2
+            },
+            {
+                "node_id": 3,
+                "player_id": "1",
+                "number_of_soldiers": 0
+            },
+            {
+                "node_id": 4,
+                "player_id": "1",
+                "number_of_soldiers": 0
+            }],
+            "spawns": [{
+                "player_id": "0",
+                "number_of_soldiers": 1,
+                "node_id": 1
+            }]
+        },
+        "11": {
+            "moves": [],
+            "states": [{
+                "node_id": 1,
+                "player_id": "0",
+                "number_of_soldiers": 17
+            },
+            {
+                "node_id": 2,
+                "player_id": "1",
+                "number_of_soldiers": 1
+            },
+            {
+                "node_id": 3,
+                "player_id": "1",
+                "number_of_soldiers": 0
+            },
+            {
+                "node_id": 4,
+                "player_id": "1",
+                "number_of_soldiers": 1
+            }],
+            "spawns": []
         }
     }
 }
