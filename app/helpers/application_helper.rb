56ffd24d88249609b2c9851cc9618530e9c15b68
Christian Blais <christ.blais@gmail.com>
2011-03-30 21:21:16 -0400

    Few modifications to layout. Hope you'll like it.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def menu
+    {
+        ArtificialIntelligence => [
+          {:action=>:index, :href=>artificial_intelligences_path},
+          {:action=>:new, :href=>new_artificial_intelligence_path}
+        ],
+
+        Game => [
+          {:action=>:index, :href=>games_path}
+        ]
+    }
+  end
 end
