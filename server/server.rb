0ab8b572bf49b9dec616b9e72bc0289cee5bcd8a
Christian Blais <christ.blais@gmail.com>
2011-04-07 22:39:35 -0400

    Server is now a Sinatra app, responding to both /random and /fight?map=MAP_ID&ais[]=AIS_ID

--- server/server.rb
+++ server/server.rb
@@ -1,4 +1,5 @@
 require 'rubygems'
+require 'sinatra'
 require 'typhoeus'
 require 'yajl/json_gem'
 require 'active_record'
@@ -21,9 +22,46 @@ ActiveRecord::Base.establish_connection( config_yaml[ environment ] )
   require File.expand_path( File.dirname( __FILE__ ) ) + "/../app/models/#{model}"
 end
 
-map = Map.first
-map.init( ArtificialIntelligence.find(:all, :limit=>2) )
+# start a random fight, on a random map with random (2!) ais
+get '/random' do
+  map = Map.first(:order=>"RAND()")
+  ais = ArtificialIntelligence.find(:all, :limit=>2, :order=>"RAND()")
+  run map, ais
+  
+  200
+end
 
-game = Game.new
-game.map = map
-game.run
+# start a fight for map X with ais Y, Z, ...
+# params[:map] : Map ID
+# params[:ais] : List of AI ids
+get '/fight' do
+  begin
+    map = Map.find(params[:map])
+    ais = ArtificialIntelligence.find(params[:ais])
+    run map, ais
+  rescue
+    log( "Error while fighting! No more info, find it yourself! ;-)" )
+
+    return 403
+  end
+
+  200
+end
+
+def run map, ais
+  map.init( ais )
+  game = Game.new
+  game.map = map
+
+  Thread.new do
+    game.run
+  end
+end
+
+def log string
+  open('errors.log', 'a') do |f|
+    f.puts "#{DateTime.now} : #{request.path_info} #{params.inspect}"
+    f.puts string
+    f.puts "---"
+  end
+end
