58ed9bb08b6fdd0b3f758cb877a130042ba1a409
Christian Blais <christ.blais@gmail.com>
2011-04-07 21:04:17 -0400

    Server load config from rails /config folder

--- server/server.rb
+++ server/server.rb
@@ -4,14 +4,17 @@ require 'yajl/json_gem'
 require 'active_record'
 require 'uuidtools'
 
+# options
+options = Hash[*ARGV]
+
 # config
-ActiveRecord::Base.establish_connection(
-  :adapter=> 'mysql',
-  :database=> 'berlin_development',
-  :username=> 'root',
-  :password=> 'password',
-  :pool=> 5,
-  :timeout=> 5000)
+default_env = ENV['RAILS_ENV'] ? ENV['RAILS_ENV'] : 'development'
+environment = options['-e'] ? options['-e'] : default_env
+config_path = File.expand_path( File.dirname( __FILE__ ) ) + "/../config/database.yml"
+config_yaml = YAML::load( File.open( config_path ) )
+
+# db connexion
+ActiveRecord::Base.establish_connection( config_yaml[ environment ] )
 
 # models
 %w( game map node_type node artificial_intelligence artificial_intelligence_game ).each do |model|
