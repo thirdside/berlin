69ce27048d752e641143951ab1288532e1ed348a
Guillaume Malette-Campeau <gmalette@gmail.com>
2011-04-09 13:31:56 -0400

    NodeJS RandomAI

---
+++ random-ai/random-ai.js
@@ -0,0 +1,238 @@
+var sys = require("sys"),
+	http = require("http"),
+	url = require("url"),
+	util = require("util"),
+	querystring = require('querystring');
+
+
+
+function RandomAI() {
+	this.initialize = function (options)
+	{
+		this.options = {
+			verbose: true,
+			port: 8080
+		};
+		
+		this.games = {};
+		
+		this.server = http.createServer(this.onRequest.bind(this)).listen(this.options.port);
+		v("Berlin Random AI Server Started");
+	}
+	
+	this.onRequest = function (request, response)
+	{
+		var uri = url.parse(request.url).pathname;
+		v("New request: " + uri);
+		if (uri === "/onturn")
+		{
+			this.onTurn(request, response);
+		} else if (uri === "/ready")
+		{
+			this.onReady(request, response);
+		}
+		response.writeHead(200, {'Content-Type': 'application/json'});
+	}
+	
+	this.onTurn = function (request, response)
+	{
+		this.postHandler(request, function(request_data)
+		{
+			var game = this.games[request_data.game];
+			var json = JSON.parse(request_data.json);
+			if (!game)
+			{
+				game = new Game();
+				game.initialize(request_data.game, json, request_data.self);
+				this.games[request_data.game] = game;
+			}
+			game.update(json);
+			
+			var moves_json = JSON.stringify(game.turnMoves());
+			response.write(moves_json);
+			v("Moves for this turn: " + moves_json);
+			
+			response.end();
+	    }.bind(this));
+	}
+	
+	this.onReady = function (request, response)
+	{
+		response.write("Ready!");
+	}
+	
+	this.calculateMoves = function (map_infos)
+	{
+		var map = {nodes:[]}
+		return map;
+	}
+	
+	this.postHandler = function (request, callback)
+	{
+		var _REQUEST = {};
+		var _CONTENT = '';
+		
+		if (request.method == 'POST')
+		{
+			request.addListener('data', function(chunk)
+			{
+				_CONTENT+= chunk;
+			});
+			
+			request.addListener('end', function()
+			{
+				_REQUEST = querystring.parse(_CONTENT);
+				callback(_REQUEST);
+			});
+		};
+	};
+};
+
+
+
+function Node ()
+{
+	this.initialize = function (id)
+	{
+		this.id = id;
+		this.links = [];
+	}
+	
+	this.linkTo = function (otherNode)
+	{
+		this.links.push(otherNode);
+	}
+	
+	this.isAdjacent = function (otherNode)
+	{
+		return this.links.indexOf(otherNode) != -1;
+	}
+	
+	this.adjacentNodes = function ()
+	{
+		return this.links;
+	}
+}
+
+
+function Game ()
+{
+	this.initialize = function (id, map_info, player_id)
+	{
+		this.id = id;
+		this.player_id = player_id;
+		this.map = new Map();
+		this.map.initialize(map_info, this.player_id);
+	}
+	
+	this.update = function (json)
+	{
+		this.map.update(json.states);
+	}
+	
+	this.turnMoves = function ()
+	{
+		var moves = [];
+		var nodes = this.map.controlledNodes();
+		v("Controlled Nodes: " + nodes);
+		for (var i = 0; i < nodes.length; i++)
+		{
+			var node = nodes[i];
+			var adj = node.adjacentNodes();
+			v("Adjacent Nodes: " + adj);
+			moves.push(
+				{
+					from: node.id, 
+					to: adj[Math.floor(Math.random()*adj.length)].id, 
+					number_of_soldiers: Math.floor(Math.random()*node.number_of_soldiers)
+				}
+			);
+		}
+		return moves;
+	}
+}
+
+
+function Map ()
+{
+	this.initialize = function (config, player_id)
+	{
+		this.player_id = player_id;
+		this.nodes = {};
+		this.directed = config.infos.directed || false;
+		
+		for (var i = 0; i < config.nodes.length; i++)
+		{
+			var node = config.nodes[i];
+			this.nodes[node.id] = new Node();
+			this.nodes[node.id].initialize(node.id);
+		}
+		
+		for (var i = 0; i < config.paths.length; i++)
+		{
+			var path = config.paths[i];
+			this.nodes[path.from].linkTo(this.nodes[path.to]);
+			
+			if (!this.isDirected())
+			{
+				this.nodes[path.to].linkTo(this.nodes[path.from]);
+			}
+		}
+	}
+	
+	this.ownedNodes = function ()
+	{
+		var nodes = [];		
+		for (i in this.nodes)
+		{
+			node = this.nodes[i];
+			if (node.player_id == this.player_id)
+			{
+				nodes.push(node);
+			}
+		}
+		
+		v("Owned Nodes: " + util.inspect(this.nodes));
+		
+		return nodes;
+	}
+	
+	this.controlledNodes = function ()
+	{
+		var nodes = [];
+		var owned_nodes = this.ownedNodes();
+		for (var i = 0; i < owned_nodes.length; i++)
+		{
+			if (owned_nodes[i].number_of_soldiers > 0)
+			{
+				nodes.push(owned_nodes[i]);
+			}
+		}
+		
+		return nodes;
+	}
+	
+	this.isDirected = function ()
+	{
+		return this.directed;
+	}
+	
+	this.update = function (states)
+	{
+		for (var i = 0; i < states.length; i++)
+		{
+			var node = this.nodes[states[i].node_id];
+			node.number_of_soldiers = states[i].number_of_soldiers;
+			node.player_id = states[i].player_id;
+		}
+	}
+}
+
+v = function (str)
+{
+	util.log(util.inspect(str));
+}
+
+
+var server = new RandomAI();
+server.initialize();
