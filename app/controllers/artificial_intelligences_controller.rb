be743607c052fc7b7ecc0450b45297fafd0e078f
Christian Blais <christ.blais@gmail.com>
2011-04-09 17:36:35 -0400

    Fix a bug while handling artificial_intelligences

--- app/controllers/artificial_intelligences_controller.rb
+++ app/controllers/artificial_intelligences_controller.rb
@@ -6,10 +6,6 @@ class ArtificialIntelligencesController < InheritedResources::Base
 
   has_scope :order, :default => "artificial_intelligences.id DESC"
 
-  def index
-    @artificial_intelligences = ArtificialIntelligence.select("artificial_intelligences.*, SUM(artificial_intelligence_games.score) AS score").joins("LEFT JOIN artificial_intelligence_games ON artificial_intelligence_games.artificial_intelligence_id = artificial_intelligences.id").order("score DESC")
-  end
-
   def create
     params[:artificial_intelligence][:user_id] = current_user.id
 
