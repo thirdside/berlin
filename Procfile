4e0e4409d3cae9bd0d1868eca5d49a367f7bb488
Guillaume Malette <gmalette@gmail.com>
2013-03-29 14:30:08 -0400

    Foreman deployment with root user

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web:    bundle exec unicorn -c config/unicorn.rb
-worker: bundle exec rake jobs:work
+web:    RAILS_ENV=production bundle exec unicorn -c config/unicorn.rb
+worker: RAILS_ENV=production bundle exec rake jobs:work
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,17 @@ require 'capistrano-unicorn'
 require 'bundler/capistrano'
 
 set :application, "berlin"
-set :repository, "." 
-set :scm, :none 
-set :deploy_via, :copy 
+set :repository, "."
+set :scm, :none
+set :deploy_via, :copy
+set :keep_releases, 5
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-set :user, :berlin
+set :user, :root
+set :app_user, :berlin
+set :app_group, :berlin
 set :use_sudo, false
 server "berlin.thirdside.ca", :app, :web, :db, :primary => true
 
@@ -25,24 +28,29 @@ set :deploy_to, "/home/berlin/sites/berlin.thirdside.ca/"
 namespace :foreman do
   desc "Export the Procfile to Ubuntu's upstart scripts"
   task :export, :roles => :app do
-    run "cd #{deploy_to}/current && #{sudo} bundle exec foreman export upstart /etc/init -a #{application} -u #{user} -l #{deploy_to}/shared/logs/upstart.log"
+    run "cd #{deploy_to}/current && #{try_sudo} bundle exec foreman export upstart /etc/init -a #{application} -u #{app_user} -l #{deploy_to}/shared/log/upstart.log"
   end
   
   desc "Start the application services"
   task :start, :roles => :app do
-    run "#{sudo} start #{application}"
+    run "#{try_sudo} start #{application}"
   end
  
   desc "Stop the application services"
   task :stop, :roles => :app do
-    run "#{sudo} stop #{application}"
+    run "#{try_sudo} stop #{application}"
   end
  
   desc "Restart the application services"
   task :restart, :roles => :app do
-    run "#{sudo} start #{application} || #{sudo} restart #{application}"
+    run "#{try_sudo} restart #{application} || #{try_sudo} start #{application}"
   end
+end
+
+task :setup_group do
+  run "chown -R #{app_user}:#{app_group} #{deploy_to} && chmod -R g+s #{deploy_to}"
 end
  
 after "deploy:update", "foreman:export"
 after "deploy:update", "foreman:restart"
+after "deploy:finalize_update", :setup_group
