532f90aa0d6beb43b1671302da26ac5e29841fd3
Guillaume Malette <guillaume.malette@jadedpixel.com>
2013-07-30 20:58:30 -0400

    Better playback with Flash
    Not cool

--- public/javascripts/simulator/playback.js
+++ public/javascripts/simulator/playback.js
@@ -6,25 +6,71 @@
  * date: 12/04/2011
  */
 
-TS.AIPlayback = Class.create(TS, {
-	initialize: function ($super, containers, map_url, game_description_url)
+TS.Playback = Class.create(TS, {
+	initialize: function($super, containers, map_url, history_url)
 	{
-		$super();
-		
-		this.game_description_url = game_description_url;
-		
+		this.containers = containers;
+		this.map_url = map_url;
+		this.history_url = history_url;
+
 		// info table row name template
 		this.playerInfoName = 'info_player_#{id}';
-		
+
+		// other shit
+		this.playerList = $(containers.player_list);
+		this.progressBar = $(containers.progress_bar);
+		this.progressTurns = $(containers.turns);
+		this.controls = $(containers.controls);
+	},
+
+	setPlayerInfo: function(id, color, soldiers, nodes, score)
+	{
+		var row = $(this.playerInfoName.interpolate({id: id}));
+		row.down('.color').setStyle({"background-color": color});
+		row.down('.soldiers').update(soldiers)
+		row.down('.nodes').update(nodes);
+		row.down('.score').update(score);
+	},
+
+	/*
+	 * Update the progress bar
+	 */
+	updateProgressPrecent: function (percent)
+	{
+		this.progressBar.setStyle("width: #{percent}%".interpolate({percent: percent}));
+	},
+
+	/*
+	 * Update the current turn #
+	 */
+	updateProgressTurns: function (turnNumber, maxTurns)
+	{
+		var format = "#{currentTurn}/#{nbTurns}";
+
+		var data =
+		{
+			currentTurn: turnNumber || 0,
+			nbTurns: maxTurns || 0,
+		};
+
+		this.progressTurns.update(format.interpolate(data));
+	},
+})
+
+TS.AIPlayback = Class.create(TS.Playback, {
+	initialize: function ($super, containers, map_url, history_url)
+	{
+		$super(containers, map_url, history_url);
+
+
 		// create the visualizer
 		this.map = new TS.AIMap(containers.map, map_url);
 		this.map.observe('ready', this.onMapReady.bindAsEventListener(this));
-		
+
 		// other shit
 		this.turnNumber = 0;
 		this.forward = true;
 		this.ready = {map:false, self:false};
-		this.controls = $(containers.controls);
 
 		// create the buttons
 		this.buttons = ["rewind", "back", "play", "pause", "next", "end"];
@@ -32,36 +78,31 @@ TS.AIPlayback = Class.create(TS, {
 			this[control] = this.controls.down("#" + control);
 			this[control].observe("click", this["on" + control.capitalize()].bindAsEventListener(this));
 		}, this);
-		
+
 		// listen to keyboard keys
 		TS.Keyboard.registerCallback(" ", [], this.togglePlayPause.bind(this));
 		TS.Keyboard.registerCallback(Event.KEY_LEFT, [], this.onBack.bindAsEventListener(this));
 		TS.Keyboard.registerCallback(Event.KEY_RIGHT, [], this.onNext.bindAsEventListener(this));
-		
+
 		// create the timer for automatic playback
 		this.timer = new TS.Timer();
 		this.timer.observe("timer", this.onTimer.bind(this));
-		
-		// other shit
-		this.playerList = $(containers.player_list);
-		this.progressBar = $(containers.progress_bar);
-		this.progressTurns = $(containers.turns);
-		
+
 		this.enableControls();
 		this.playbackDescription = null;
 		this.stepTime = 500;
 	},
-	
+
 	/*
 	 * Called when the game description is fetched from the server
 	 */
 	onGameDescriptionLoaded: function (request)
-	{	
+	{
 		// parse the answer
-		this.gameDescription = request.responseText.evalJSON();		
-	
-		this.ready.self = true;	
-		
+		this.gameDescription = request.responseText.evalJSON();
+
+		this.ready.self = true;
+
 		// draw the first turn (setup)
 		if (this.isReady()) {
 			// create the playback description
@@ -72,13 +113,13 @@ TS.AIPlayback = Class.create(TS, {
 				this.map.layers['background'],
 				this.map.graphics,
 				this.stepTime);
-			this.playbackDescription.initializeTurns();		
-			
+			this.playbackDescription.initializeTurns();
+
 			this.enableControls();
 			this.drawCurrentTurn();
 		}
-	},	
-	
+	},
+
 	/*
 	 * Button callback: Rewind
 	 */
@@ -89,7 +130,7 @@ TS.AIPlayback = Class.create(TS, {
 		this.forward = false;
 		this.drawCurrentTurn();
 	},
-	
+
 	/*
 	 * Button callback: Back
 	 */
@@ -104,7 +145,7 @@ TS.AIPlayback = Class.create(TS, {
 			this.drawCurrentTurn();
 		}
 	},
-	
+
 	/*
 	 * Button callback: Play
 	 */
@@ -112,11 +153,11 @@ TS.AIPlayback = Class.create(TS, {
 	{
 		if (this.turnNumber >= this.getMaxTurn())
 			this.turnNumber = 0;
-		
+
 		this.start();
 		this.enableControls();
 	},
-	
+
 	/*
 	 * Button callback: Pause
 	 */
@@ -125,7 +166,7 @@ TS.AIPlayback = Class.create(TS, {
 		this.stop();
 		this.enableControls();
 	},
-	
+
 	/*
 	 * Toggle between the Play/Pause buttons
 	 */
@@ -134,7 +175,7 @@ TS.AIPlayback = Class.create(TS, {
 		e.stop();
 		this.timer.isRunning()? this.onPause() : this.onPlay();
 	},
-	
+
 	/*
 	 * Button callback: Next
 	 */
@@ -149,7 +190,7 @@ TS.AIPlayback = Class.create(TS, {
 			this.drawCurrentTurn();
 		}
 	},
-	
+
 	/*
 	 * Button callback: End
 	 */
@@ -162,7 +203,7 @@ TS.AIPlayback = Class.create(TS, {
 
 	/*
 	 * Draw the current frame of the simulation
-	 */	
+	 */
 	drawCurrentTurn: function ()
 	{
 		// draw the current frame
@@ -175,47 +216,27 @@ TS.AIPlayback = Class.create(TS, {
 		this.enableControls();
 		this.updatePlayerList();
 		this.updateProgress();
-		this.updateProgressTurns();
 	},
-	
-	/*
-	 * Update the progress bar
-	 */
-	updateProgress: function ()
+
+	updateProgress: function()
 	{
-		this.progressBar.setStyle("width: #{percent}%".interpolate({percent: this.turnNumber/this.getMaxTurn() * 100}));
+		this.updateProgressPercent(this.turnNumber/this.getMaxTurn() * 100);
+		var currentTurn = (this.turnNumber / this.playbackDescription.simulatioGameRatio) || 0
+		var nbTurns = (this.getMaxTurn() / this.playbackDescription.simulatioGameRatio) || 0
+
+		this.updateProgressTurns(currentTurn, maxTurns);
 	},
-	
-	/*
-	 * Update the current turn #
-	 */
-	updateProgressTurns: function ()
-	{
-		var format = "#{currentTurn}/#{nbTurns}";
-		
-		var data =
-		{
-			currentTurn: this.turnNumber / this.playbackDescription.simulatioGameRatio | 0,
-			nbTurns: this.getMaxTurn() / this.playbackDescription.simulatioGameRatio | 0
-		};
-		
-		this.progressTurns.update(format.interpolate(data));
-	},
-	
+
 	/*
 	 * Upgrade the player infos
 	 */
 	updatePlayerList: function ()
 	{
 		Object.values(this.playbackDescription.turns[Math.min(this.turnNumber, this.getMaxTurn() - 1)].players).each(function(player){
-			var row = $(this.playerInfoName.interpolate(player));
-			row.down('.color').setStyle({"background-color": player.color});
-			row.down('.soldiers').update(player.soldiers)
-			row.down('.nodes').update(player.nodes);
-			row.down('.score').update(player.score);
+			this.setPlayerInfo(player.id, player.color, player.soldiers, player.nodes, player.score);
 		}, this);
 	},
-	
+
 	/*
 	 * Get the number of simulation turns.
 	 * A simulation turn is 1/5 of a game turn.
@@ -224,7 +245,7 @@ TS.AIPlayback = Class.create(TS, {
 	{
 		return this.playbackDescription.turns.length;
 	},
-	
+
 	/*
 	 * Called when the visualizer is ready
 	 * i.e. the config was succesfully loaded
@@ -232,11 +253,11 @@ TS.AIPlayback = Class.create(TS, {
 	onMapReady: function ()
 	{
 		this.ready.map = true;
-		
+
 		// request the game description
-		new Ajax.Request( this.game_description_url, {method: 'get', onComplete: this.onGameDescriptionLoaded.bindAsEventListener(this)});
+		new Ajax.Request( this.history_url, {method: 'get', onComplete: this.onGameDescriptionLoaded.bindAsEventListener(this)});
 	},
-	
+
 	/*
 	 * Checks if the playback is ready
 	 * i.e. the visualizer and the playback are ready.
@@ -245,7 +266,7 @@ TS.AIPlayback = Class.create(TS, {
 	{
 		return $H(this.ready).all();
 	},
-	
+
 	/*
 	 * Set the controls depending on the state of the simulation
 	 */
@@ -254,10 +275,10 @@ TS.AIPlayback = Class.create(TS, {
 		this.buttons.each(function(control){
 			this[control].disabled = this.isReady() ? "" : "disabled";
 		},this);
-		
+
 		this.play[this.timer.isRunning() ? "hide" : "show"]();
 		this.pause[this.timer.isRunning() ? "show" : "hide"]();
-		
+
 		if (this.turnNumber == 0)
 		{
 			this.back.disabled = "disabled";
@@ -268,7 +289,7 @@ TS.AIPlayback = Class.create(TS, {
 			this.end.disabled = "disabled;"
 		}
 	},
-	
+
 	/*
 	 * Stop the automatic playback
 	 */
@@ -276,7 +297,7 @@ TS.AIPlayback = Class.create(TS, {
 	{
 		this.timer.stop();
 	},
-	
+
 	/*
 	 * Start the automatic playback
 	 */
@@ -284,22 +305,52 @@ TS.AIPlayback = Class.create(TS, {
 	{
 		this.timer.start(this.stepTime, -1);
 	},
-	
+
 	/*
 	 * Process one turn in automatic playback
 	 */
 	onTimer: function ()
 	{
 		this.forward = true;
-		
+
 		if (this.timer.isRunning())
 			this.turnNumber++;
-		
+
 		this.drawCurrentTurn();
-		
+
 		if (this.turnNumber >= this.getMaxTurn()) {
 			this.timer.stop();
 			return;
-		}		
+		}
 	}
 });
+
+TS.FlashPlayback = Class.create(TS.Playback, {
+	initialize: function($super, containers, map_url, history_url)
+	{
+		$super(containers, map_url, history_url);
+		this.setupFlash();
+	},
+
+	setupFlash: function()
+	{
+		// For version detection, set to min. required Flash Player version, or 0 (or 0.0.0), for no version detection.
+    var swfVersionStr = "10.2.0";
+    // To use express install, set to playerProductInstall.swf, otherwise the empty string.
+    var flashvars = {map: this.map_url, history: this.history_url};
+    var params = {};
+    params.quality = "high";
+    params.bgcolor = "#202020";
+    params.allowscriptaccess = "always";
+    params.allowfullscreen = "true";
+    var attributes = {};
+    attributes.id = "berlin-client";
+    attributes.name = "berlin-client";
+    attributes.align = "middle";
+    swfobject.embedSWF(
+        "/berlin-client.swf", this.containers.flash,
+        "960", "500",
+        swfVersionStr, null,
+        flashvars, params, attributes);
+	},
+})
