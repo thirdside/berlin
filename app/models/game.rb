7ca112cad0154d5b8b4427daf93b21257c0e7af1
Christian Blais <christ.blais@gmail.com>
2013-08-14 22:56:44 -0400

    Ensure is_practice is set before saving

--- app/models/game.rb
+++ app/models/game.rb
@@ -13,6 +13,8 @@ class Game < ActiveRecord::Base
 
   after_create :send_notification
 
+  before_save :ensure_is_practice_is_set
+
   def number_of_players
     artificial_intelligence_games_count
   end
@@ -28,6 +30,11 @@ class Game < ActiveRecord::Base
   end
 
   protected
+
+    def ensure_is_practice_is_set
+      self.is_practice = false if self.is_practice.nil?
+    end
+
     def send_notification
       self.artificial_intelligences.map(&:user_id).uniq.each do |user|
         Notification.push user, self
