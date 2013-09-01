8071d11e96dcf62d550d3bc17bdca28cdd42af05
Guillaume Malette <guillaume@jadedpixel.com>
2013-08-31 21:09:17 -0400

    Change method names

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -3,7 +3,7 @@ class TournamentsController < ApplicationController
   respond_to :json, :html
 
   before_filter :ensure_logged_in, :only => [:edit, :update, :create, :new]
-  before_filter :verify_ownership, :only => [:edit, :update]
+  before_filter :ensure_ownership, :ensure_not_started, :only => [:edit, :update]
 
   def create
     @tournament = current_user.tournaments.build(params[:tournament])
@@ -11,9 +11,15 @@ class TournamentsController < ApplicationController
   end
 
   protected
-  def verify_ownership
+  def ensure_ownership
     unless current_user == resource.user
       redirect_to resource, :alert => t("tournaments.cannot_edit_flash")
+    end
+  end
+  
+  def ensure_not_started
+    if resource.rounds.any?
+      redirect_to resource, :alert => t("tournaments.already_started")
     end
   end
 end
