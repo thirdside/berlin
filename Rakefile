abb1dfdb6dbdd6b7b60c0776c52b283b81624981
Guillaume Malette <guillaume@jadedpixel.com>
2013-09-04 20:35:32 -0400

    Fix deployment for 4.0

--- Rakefile
+++ Rakefile
@@ -10,4 +10,22 @@ rescue LoadError
   STDERR.puts "Rn `bundle install` to install delayed_job"
 end
 
+
+namespace :assets do
+  task nodigest: :environment do
+    assets = Dir.glob(File.join(Rails.root, 'public/assets/**/*'))
+    regex = /(-{1}[a-z0-9]{32}*\.{1}){1}/
+    assets.each do |file|
+      next if File.directory?(file) || file !~ regex
+
+      source = file.split('/')
+      source.push(source.pop.gsub(regex, '.'))
+
+      non_digested = File.join(source)
+      FileUtils.cp(file, non_digested)
+    end
+  end
+end
+
+
 BerlinWeb::Application.load_tasks
--- config/deploy.rb
+++ config/deploy.rb
@@ -24,23 +24,23 @@ set :deploy_to, "/home/berlin/sites/berlin.thirdside.ca/"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
- 
+
 namespace :foreman do
   desc "Export the Procfile to Ubuntu's upstart scripts"
   task :export, :roles => :app do
     run "cd #{deploy_to}/current && #{try_sudo} bundle exec foreman export upstart /etc/init -a #{application} -u #{app_user} -l #{deploy_to}/shared/log/upstart.log"
   end
-  
+
   desc "Start the application services"
   task :start, :roles => :app do
     run "#{try_sudo} start #{application}"
   end
- 
+
   desc "Stop the application services"
   task :stop, :roles => :app do
     run "#{try_sudo} stop #{application}"
   end
- 
+
   desc "Restart the application services"
   task :restart, :roles => :app do
     run "#{try_sudo} restart #{application} || #{try_sudo} start #{application}"
@@ -50,7 +50,16 @@ end
 task :setup_group do
   run "chown -R #{app_user}:#{app_group} #{deploy_to} && chmod -R g+s #{deploy_to}"
 end
- 
+
+namespace :deploy do
+  namespace :assets do
+    task :nodigest do
+      run("cd #{current_release} && bundle exec rake assets:nodigest RAILS_ENV=production RAILS_GROUPS=assets")
+    end
+  end
+end
+
 after "deploy:update", "foreman:export"
 after "deploy:update", "foreman:restart"
 after "deploy:finalize_update", :setup_group
+after "deploy:assets:precompile", "deploy:assets:nodigest"
