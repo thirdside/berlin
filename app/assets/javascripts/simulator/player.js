15b369ca6d924966bb54ef7ee22567e604ff033e
Guillaume Malette <guillaume@jadedpixel.com>
2013-09-07 10:42:24 -0400

    Change AI colors

--- app/assets/javascripts/simulator/player.js
+++ app/assets/javascripts/simulator/player.js
@@ -11,56 +11,44 @@ TS.Player = Class.create(TS, {
 	{
 		this.id = id;
 		this.color = this._initPlayerColor(id);
-		
+
 		this.soldiers = 0;
 		this.nodes = 0;
 		this.score = 0;
 	},
-	
+
 	sync: function (map)
 	{
 		this.soldiers = 0;
 		this.nodes = 0;
 		this.score = 0;
-		
+
 		Object.keys(map.nodes).each(function(nodeId) {
 			var node = map.nodes[nodeId];
-		
+
 			// sync score
 			if (this.id == node.playerId)
 				this.score += node.value;
-				
+
 			// sync nodes
 			if (this.id == node.playerId)
 				this.nodes += 1;
-		
+
 			// sync soldiers
 			var movesIds = node.players.keys();
-			
+
 			movesIds.each(function(id) {
 				if (this.id == id)
 					this.soldiers += node.players.get(id);
 			}, this);
 		}, this);
 	},
-	
+
 	/*
 	 * Get the player's color (for a known player)
 	 */
 	_initPlayerColor: function (playerId)
 	{
-		var color = '#FFFFFF';
-		
-		switch (playerId)
-		{
-			case 0: color = '#bd1550'; break;
-			case 1: color = '#e97f02'; break;
-			case 2: color = "#73797b"; break;
-			case 3: color = "#e32424"; break;
-			case 4: color = "#21bbbd"; break;
-			case 5: color = "#8a9b0f"; break;
-		}
-		
-		return color;
-	}	
+		return ['#bd1550', '#e97f02', '#50a2e9', '#f31474', '#21bbad', '#8a9b0f'][playerId] || '#ffffff'
+	}
 });
