cf1ce3c494e7bd78e1a03b16dcc4690a0a4a6597
Jodi Giordano <jodi@ephemeregames.com>
2013-09-08 14:51:03 -0400

    Added debug layer.

--- app/assets/javascripts/simulator/drawable.js
+++ app/assets/javascripts/simulator/drawable.js
@@ -7,14 +7,14 @@
  */
 
  TS.Drawable = Class.create(TS, {
-	initialize: function (container, position, size, translate)
+	initialize: function (container, options)
 	{
 		this.container = container;
-		this.position = Object.extend({x: 0, y: 0}, position || {});
-		this.size = Object.extend({width: 1, height: 1}, size || {});
-		this.translate = translate;
+		this.position = Object.extend({x: 0, y: 0}, options.position || {});
+		this.size = Object.extend({width: 1, height: 1}, options.size || {});
+		this.translate = options.translate;
 	},
-	
+
 	getRelativePosition: function (position)
 	{
 		var pos =
@@ -22,13 +22,13 @@
 			x: position.x - this.translate.x,
 			y: position.y - this.translate.y
 		};
-		
+
 		pos.x = (pos.x / this.size.width) * 100;
 		pos.y = (pos.y / this.size.height) * 100;
-		
+
 		return pos;
 	},
-	
+
 	getAbsolutePosition: function (position)
 	{
 		var pos =
@@ -36,13 +36,13 @@
 			x: position.x / 100 * this.size.width,
 			y: position.y / 100 * this.size.height
 		};
-		
+
 		pos.x += this.translate.x;
 		pos.y += this.translate.y;
-		
+
 		return pos;
 	},
-	
+
 	getQuadraticCurvePoint: function (from, to, ratio)
 	{
 		var x =
@@ -50,7 +50,7 @@
 			x: (from.x + (to.x - from.x) / 2 + (to.y - from.y) * ratio),
 			y: (from.y + (to.y - from.y) / 2 + (to.x - from.x) * ratio * -1)
 		};
-		
+
 		return x;
 	},
 });
