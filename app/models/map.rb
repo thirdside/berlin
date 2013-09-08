61b05f1e3b93fce00b895777f77abedcbbab344d
Guillaume Malette <guillaume@jadedpixel.com>
2013-09-08 17:23:00 -0400

    Fix map#show

--- app/models/map.rb
+++ app/models/map.rb
@@ -3,7 +3,11 @@ class Map < ActiveRecord::Base
   has_many :artificial_intelligence_games, :through => :games
 
   def best_artificial_intelligence
-    self.artificial_intelligence_games.group("artificial_intelligence_id").select("SUM(artificial_intelligence_games.score) / COUNT(artificial_intelligence_games.id) AS ratio").order("ratio DESC").first.try(:artificial_intelligence)
+    self.artificial_intelligence_games
+      .group("artificial_intelligence_id")
+      .select("SUM(artificial_intelligence_games.score) / COUNT(artificial_intelligence_games.id) AS ratio, artificial_intelligence_id")
+      .order("ratio DESC")
+      .first.try(:artificial_intelligence)
   end
 
   def last_game
