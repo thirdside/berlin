e2ba755fed47d5e36c4c6d67e36185a07067e46e
Guillaume Malette <guillaume@jadedpixel.com>
2013-09-12 21:11:13 -0400

    Create user through API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,24 @@
 class UsersController < ApplicationController
   inherit_resources
+  respond_to :html
+  respond_to :json, :only => [:create, :update, :show, :index]
 
   include Pageable
 
   has_scope :order, :default => "users.username"
-  
+
   before_filter :current_user_only, :only => [:edit, :destroy, :update]
+
 
   def artificial_intelligences
     @user = User.find(params[:id])
-    
+
     @artificial_intelligences = @user.artificial_intelligences
   end
-  
+
   def games
     @user = User.find(params[:id])
-    
+
     @games = @user.games
   end
 
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
+  test ":create creates a user using json" do
+    basic_auth
+    post :create, :user => {:username => "Toto", :email => "email@toto.com", :password => "totototo"}, :format => :json
+
+    assert_response :created
+
+    user = ActiveSupport::JSON.decode(response.body)['user']
+
+    assert_equal "Toto", user['username']
+    assert_equal [], user['artificial_intelligences']
+  end
+end
