122fa037e01a060f7348a7178ffc283ab7a8e83b
Guillaume Malette <guillaume@jadedpixel.com>
2013-09-12 21:09:30 -0400

    Deprecated map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -6,7 +6,7 @@ class MapsController < ApplicationController
   include Pageable
 
   def show
-    @map = Map.find(params[:id], :include => {:games => :artificial_intelligence_games})
+    @map = Map.includes(:games => :artificial_intelligence_games).find(params[:id])
     @games = @map.games.officials.page(params[:page])
 
     respond_to do |format|
